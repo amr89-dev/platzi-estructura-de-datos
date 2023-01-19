fe7ffa50811caee22977a15b6d0a5d7c558992ab
sorrus-dev <amr89.dev@gmail.com>
2023-01-18 20:11:13 -0500

    metodos delete y shiftItem

--- array.js
+++ array.js
@@ -14,9 +14,24 @@ class MyArray {
     return this.data;
   }
   pop() {
+    const lastItem = this.data[this.length - 1];
     delete this.data[this.length - 1];
     this.length--;
     return this.data;
+  }
+  delete(index) {
+    const item = this.data[index];
+    console.log("delete-->", item);
+    this.shiftIndex(index);
+    return item;
+  }
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+      console.log("->", this.data[i]);
+    }
+    delete this.data[this.length - 1];
+    this.length--;
   }
 }
 
